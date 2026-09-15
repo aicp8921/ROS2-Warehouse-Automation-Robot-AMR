6e7875119c9adfd1dd13a4396b6bfd79eb17fb4d
ashfakh korothil <mohammedashfakhtirur@gmail.com>
2026-09-15 12:59:54 +0530

    URDF upfated.

---
+++ war_ws/src/robot_description/robot_description/launch/display.launch.py
@@ -0,0 +1,42 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+import xacro
+
+def generate_launch_description():
+
+    package_name = 'robot_description'
+    urdf_file = os.path.join(
+        get_package_share_directory(package_name),
+        'urdf', 
+        'robot.urdf.xacro'
+    )
+
+    with open(urdf_file, 'r', encoding='utf-8') as file:
+        robot_description = xacro.process_file(urdf_file).toxml()
+
+    robot_state_publisher_node = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[{'robot_description': robot_description}]
+    )
+
+    joint_state_publisher_gui_node = Node(
+        package='joint_state_publisher_gui',
+        executable='joint_state_publisher_gui',
+        output='screen'
+    )
+
+    rviz_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        output='screen',
+    )
+
+    return LaunchDescription([
+        robot_state_publisher_node,
+        joint_state_publisher_gui_node,
+        rviz_node
+    ])
