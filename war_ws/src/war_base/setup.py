710644ec8d210e94c7d3a755fbc30f97ff8c25a7
Abdulla Ishaq <ishaqtva@gmail.com>
2026-09-18 15:59:55 +0530

    war_base pkg created & odom_publisher.py made

---
+++ war_ws/src/war_base/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+package_name = 'war_base'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='ishaqcp',
+    maintainer_email='ishaqtva@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+    'console_scripts': [
+        'odom_publisher = war_base.odom_publisher:main',
+    ],
+},
+)
